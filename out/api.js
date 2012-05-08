952ddb645bb79f9b732919c2d1a7ee137b3ee7dd
Cameron Prebble <cameron.prebble@abletech.co.nz>
2012-05-09 10:46:41 +1200

    investigate docs

---
+++ out/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "NeatComplete.Widget"
+    ],
+    "modules": [
+        "NeatComplete"
+    ],
+    "allModules": [
+        {
+            "displayName": "NeatComplete",
+            "name": "NeatComplete"
+        }
+    ]
+} };
+});
