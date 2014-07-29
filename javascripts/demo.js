decd902c456ce6471918aafe71090ff2fc65701a
Cameron Prebble <cameron.prebble@abletech.co.nz>
2014-07-30 11:21:58 +1200

    refactor jumbotron

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-  widget = new NeatComplete.Widget(document.getElementById('location'),{footerContent:"search powered by GeoNames.",emptyContent:"No results found."})
-  widget.addService("test",function(query,response_fn){
-    $.ajax({
-      url: "http://ws.geonames.org/searchJSON",
-      dataType: "jsonp",
-      data: {featureClass: "P", style: "full", maxRows: 12, name_startsWith: query, username: 'cameron_prebble' },
-      success: function( data ) {
-        response_fn(query, $.map( data.geonames, function( item ) {
-          return {
-            value: item.name + ((item.adminName1 && item.adminName1 != item.name) ? ", " + item.adminName1 : "") + ", " + item.countryName,
-            score: item.score,
-            data: item
-          }
-        }));
-      }
-    });
+  var widget = new NeatComplete.Widget(document.getElementById('location'),{footer_content:"search powered by GeoNames.",empty_content:"No results found."})
+  var service = widget.addService("test",function(query,response_fn){
+    var options = {
+      url: "http://ws.geonames.org/searchJSON",dataType: "jsonp",
+      data: {featureClass: "P", style: "full", maxRows: 12, name_startsWith: query, username: 'cameron_prebble' }
+    }
+    var success_fn = function(data) {
+      response_fn(query, $.map( data.geonames, function( item ) {
+        return {
+          value: item.name + ((item.adminName1 && item.adminName1 != item.name) ? ", " + item.adminName1 : "") + ", " + item.countryName,
+          score: item.score,
+          data: item
+        }
+      }));
+    }
+    $.ajax(options).done(success_fn);
   });
 });
