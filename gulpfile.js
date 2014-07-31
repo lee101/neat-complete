bfb9e0b59d91b143589c0d19f9f1a22c59e9ed7e
Cameron Prebble <cameron.prebble@abletech.co.nz>
2014-08-01 11:37:05 +1200

    setup gulp qunit tests

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    qunit = require('gulp-qunit');
 
 
 gulp.task('coffee', function(){
@@ -25,9 +26,14 @@ gulp.task('scss', function(){
     .pipe(gulp.dest('./lib/'))
 });
 
+gulp.task('test', function(){
+  gulp.src('./test/index.html')
+    .pipe(qunit())
+});
+
 gulp.task('watch', function(){
-  gulp.watch('./src/coffee/*.coffee', ['coffee']);
+  gulp.watch('./src/coffee/*.coffee', ['coffee', 'test']);
   gulp.watch('./src/scss/*.scss', ['scss'])
 });
 
-gulp.task('default', ['compile']);
+gulp.task('default', ['coffee', 'scss', 'test']);
