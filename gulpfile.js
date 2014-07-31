bb7e414ebf4fba4acf22d99d5b7aecc9d0e1a24f
Cameron Prebble <cameron.prebble@abletech.co.nz>
2014-08-01 11:49:47 +1200

    add dev server to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,11 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
-    qunit = require('gulp-qunit');
+    qunit = require('gulp-qunit'),
+    connect = require('gulp-connect');
 
-
+/* Main coffescript build task. Combines coffeescript files, compiles them, then
+uglifies the compiled file  */
 gulp.task('coffee', function(){
   gulp.src('./src/coffee/core.coffee')
     .pipe(preprocess())
@@ -19,6 +21,7 @@ gulp.task('coffee', function(){
     .pipe(gulp.dest('./lib/'));
 });
 
+/* Compile scss to css */
 gulp.task('scss', function(){
   gulp.src('./src/scss/style.scss')
     .pipe(sass())
@@ -26,14 +29,22 @@ gulp.task('scss', function(){
     .pipe(gulp.dest('./lib/'))
 });
 
+/* Runs qunit tests */
 gulp.task('test', function(){
   gulp.src('./test/index.html')
     .pipe(qunit())
 });
 
+/* Watches for changes to recompile coffeescript and run tests */
 gulp.task('watch', function(){
   gulp.watch('./src/coffee/*.coffee', ['coffee', 'test']);
-  gulp.watch('./src/scss/*.scss', ['scss'])
+  gulp.watch('./src/scss/*.scss', ['scss']);
+});
+
+/* Creates a server for development */
+gulp.task('connect', function(){
+  connect.server({port: 8000});
 });
 
 gulp.task('default', ['coffee', 'scss', 'test']);
+gulp.task('serve', ['connect', 'watch']);
